3415d66633d72c17d851bb9768d2406fd837b727
Froi Nunez <froinunezm@gmail.com>
2018-01-21 20:07:13 -0500

    Initialized vuejs project

---
+++ src/components/Faq.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="faq">
+    Faq
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Faq',
+  data () {
+    return {
+      msg: 'Hello world'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+  @import '../responsive'
+</style>
