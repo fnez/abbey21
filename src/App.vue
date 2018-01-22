3010d3381221435f76c2c0042016d2876ab98b45
Froi Nunez <froinunezm@gmail.com>
2018-01-21 23:07:43 -0500

    colors on nav, spacing added

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,20 @@
 
 <template>
   <div id="app">
-    <nav class="navbar" role="navigation">
-      <div class="navbar-brand">
-        <a href="https://bulma.io" class="navbar-item">
-          <img src="./assets/furniture-store.png" alt="A furniture store in Charlote, NC">
-          <h1 class="logo-title">Abbey 21st Furniture</h1>
-        </a>
+    <nav class="navbar bd-special-shadow" role="navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a href="/" class="navbar-item">
+            <span class="logo">A21</span>
+            <h1 class="logo-title">Abbey 21st Furniture</h1>
+          </a>
 
-        <button class="button navbar-burger">
+          <button class="button navbar-burger">
             <span></span>
             <span></span>
             <span></span>
           </button>
-      </div>
+        </div>
 
         <div class="navbar-menu">
           <div class="navbar-end">
@@ -23,11 +24,15 @@
             <router-link class="navbar-item"  to="/contact">Contact</router-link>
             <router-link class="navbar-item"  to="/faq">FAQ/Preguntas</router-link>
             <router-link class="navbar-item" to="apply">
-              <button class="button is-primary is-outlined">Credit Apply</button>
+              <button class="button is-primary is-outlined">
+                <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+                Credit Apply
+              </button>
             </router-link>
 
           </div>
         </div>
+      </div>
     </nav>
     
     <!-- CONTENT FROM ROUTER -->
@@ -48,12 +53,34 @@ export default {
 @import '_responsive'
 
 #app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  h1.logo-title 
-    font-weight: bold
-    margin-left: 10px
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  nav.navbar 
+    background-color: #fff
+    div.container
+      padding: 20px
+      max-width: 1200px
+      margin: 0 auto
+      div.navbar-brand
+        span.logo
+          background-color: $primary
+          border-radius: 50px
+          padding: 10px
+          color: #fff
+          font-weight: bold
+        h1.logo-title 
+          font-weight: bold
+          margin-left: 10px
+          font-size: 25px
+      div.navbar-menu
+        div.navbar-end
+          .navbar-item
+            &:hover
+              background-color: initial
+              color: $primary
+            .button i 
+              margin: 0 6px
 </style>
