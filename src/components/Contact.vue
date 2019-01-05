f4107a5383dcd7bad2058165fe607b76792a70bd
fnez <froinunezm@gmail.com>
2019-01-05 14:06:46 -0500

    Update image folder, contact.vue template start

---
+++ src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="contact"></div>
+</template>
+
+<script>
+    export default {
+        name: 'Contact',
+        components: {},
+        data() {
+            return {
+                title: 'Contact'
+            }
+        }
+    }
+</script>
+
+<style scoped lang="sass">
+    @import '../responsive'
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div id="banner" v-bind:style="{backgroundImage: 'url(' + mainImg + ')'}"></div>
+    <div id="banner" :style="{'background-image': 'url(' + require('../assets/images/living_room/wall_sofa_furniture.jpg') + ')'}"></div>
 
     <div class="content-container" v-for="item in intro">
       <div class="inner-container">
@@ -8,13 +8,14 @@
         <div class="row">
           <img v-for="el in item.images" v-bind:src="el" alt="intro-furniture">
         </div>
-        <p>{{ item.content }}</p>    
+        <p>{{ item.copy }}</p>
       </div>
     </div>
 
     <div class="content-container gray-bg">
       <div class="inner-container column">
           <div v-for="item in sections">
+            <img :src="item.image" :alt="item.alt">
             <h2>{{ item.title }}</h2>
             <p>{{ item.content }}</p>
         </div>
@@ -29,12 +30,9 @@ export default {
   name: 'Home',
   data () {
     return {      
-
-      mainImg: 'https://picsum.photos/1200/800',
-      // mainImg: '../assets/images/livingroom_main.jpg',
       intro: [
-        { msg: 'Welcome to Your Vue.js App'},
-        { content: 'Dolor nunc vule putateulr ips dol consem donec semp ertet laciniate ultricie upien disse comete dolo lectus fgillaitollicil tua ludin dolor met quam accumsan ipsun. Dolore con dime netus lullam utlacusadipi scing ipsum molestie euismod lore estibulum vel libero ipsum sit amet sollicitudin ante. Aenean imperdiet aliquet hendrerit. Nunc interdum ullamcorper lectuset pellentesque enim interdum at. Suspendisse malesuada dignissim est, a facilisis ligula rutrum sed.Dolor nunc vule putateulr ips dol consem donec semp ertet laciniate ultricie upien disse comete dolo lectus fgillaitollicil tua ludin dolor met quam accumsan ipsun.Dolore con dime netus lullam utlacusadi pi scing ipsum.'},
+        { msg: 'Welcome to Abbey21st Furniture\'s Website'},
+        { copy: 'Dolor nunc vule putateulr ips dol consem donec semp ertet laciniate ultricie upien disse comete dolo lectus fgillaitollicil tua ludin dolor met quam accumsan ipsun. Dolore con dime netus lullam utlacusadipi scing ipsum molestie euismod lore estibulum vel libero ipsum sit amet sollicitudin ante. Aenean imperdiet aliquet hendrerit. Nunc interdum ullamcorper lectuset pellentesque enim interdum at. Suspendisse malesuada dignissim est, a facilisis ligula rutrum sed.Dolor nunc vule putateulr ips dol consem donec semp ertet laciniate ultricie upien disse comete dolo lectus fgillaitollicil tua ludin dolor met quam accumsan ipsun.Dolore con dime netus lullam utlacusadi pi scing ipsum.'},
         {        
           images: [
           'https://picsum.photos/1200/800',
@@ -45,18 +43,21 @@ export default {
       ],
       sections: [
         { 
-          image: 'https://picsum.photos/1200/800',
+          image: '../assets/images/bedroom/artesia_bedroom_set_02.png',
+          alt: 'Living Room',
           title: 'Living Room',
           content: 'Dolor nunc vule putateulr ips dol consem donec semp ertet laciniate ultricie upien disse comete dolo lectus fgillaitollicil tua ludin dolor met quam accumsan ipsun. Dolore con dime netus lullam utlacusadipi scing ipsum molestie euismod lore estibulum vel'
         },
         { 
           image: 'https://picsum.photos/1200/800',
+          alt: 'Bedroom',
           title: 'Bedroom',
           content: 'Dolor nunc vule putateulr ips dol consem donec semp ertet laciniate ultricie upien disse comete dolo lectus fgillaitollicil tua ludin dolor met quam accumsan ipsun. Dolore con dime netus lullam utlacusadipi scing ipsum molestie euismod lore estibulum vel'
         },
         { 
           image: 'https://picsum.photos/1200/800',
-          title: 'Living Room',
+          alt: 'Office',
+          title: 'Office',
           content: 'Dolor nunc vule putateulr ips dol consem donec semp ertet laciniate ultricie upien disse comete dolo lectus fgillaitollicil tua ludin dolor met quam accumsan ipsun. Dolore con dime netus lullam utlacusadipi scing ipsum molestie euismod lore estibulum vel'
         },        
       ]
@@ -73,7 +74,9 @@ export default {
   position: relative
   #banner
     height: 500px
-    background: no-repeat center center fixed
+    background-repeat: no-repeat 
+    background-attachment: fixed
+    background-position: 10px -250px
     background-size: cover
     left: 0
     right: 0
@@ -83,7 +86,7 @@ export default {
   .content-container
     .inner-container
       max-width: 800px
-      margin: 0 auto
+      margin: 30px auto
       .row
         display: flex
         flex-direction: row
